d8e6303044bc2f069f2232ce09477445e20f81cb
young285 <honeet12@gmail.com>
2026-08-07 21:26:33 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='dream-key-pro-shell-v11.0';
+const CACHE='dream-key-pro-shell-v12.0';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
